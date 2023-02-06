8399dd1ee22ebdd4d70c4480f0cd1f035edf1e40
AqueelAhmedV <aqueelvallanchira13@gmail.com>
2023-02-06 22:28:37 +0530

    edit

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -74,8 +74,8 @@ const Table = (props) => {
 					</button>
 				</div>
 			) : (
-				<div className="max-w-500">
-					<div className="relative overflow-x-scroll shadow-md sm:rounded-lg">
+				<div className="max-w-400">
+					<div className="relative overflow-x-auto shadow-md sm:rounded-lg">
 						<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
 							<thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
 								<tr>
