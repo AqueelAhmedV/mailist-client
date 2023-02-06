48d343c24ebe4dd0f2af5c93e28b761dce1d0977
AqueelAhmedV <aqueelvallanchira13@gmail.com>
2023-02-06 20:15:19 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Table from './components/Table'
+
+
+function App() {
+  const [isAuthed, setAuthed] = useState(false);
+  const [data, setData] = useState([]);
+  const [verifying, setVerifying] = useState(false);
+  const [errors, setErrors] = useState([]);
+  
+
+  const baseUrl = "http://localhost:5000/api/admin";
+
+  useEffect(() => {
+    if(localStorage.getItem('token')){
+      if(Date.now() < parseInt(localStorage.getItem('loginTime'))+21600*1000)
+        setAuthed(true)
+    }
+  })
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    var pass = e.target.elements[0].value;
+    // console.log(pass);
+    setVerifying(true);
+    axios
+      .post(
+        `${baseUrl}/login`,
+        {
+          name: "FeliQ",
+          password: pass,
+        }
+      )
+      .then((response) => {
+        setVerifying(false)
+        setAuthed(true)
+        localStorage.setItem("token", response.data.token);
+        localStorage.setItem('loginTime',Date.now())
+      })
+      .catch((err) => {
+        setVerifying(false)
+        setErrors([...errors, err.response]) 
+        console.log(err.response.data)
+      });
+  };
+
+  // useEffect(() => {setTimeout(() => setErrors([]), 5000)},[errors.length>0])
+
+  return (
+    <div className="App max-w-400 p-4 flex justify-center items-center h-screen">
+      { isAuthed ? (<Table baseUrl={baseUrl} setAuthed={setAuthed}/>) : (
+        <div>
+          
+          <h1 className="text-center text-xl text-blue-900 pb-7">Admin Portal</h1>
+          {(errors.length!==0)?<div
+              className="bg-red-100 rounded border-red-500 text-red-700 px-3 py-2 mb-2"
+              role="alert"
+            >
+              <p className="font-bold text-sm">{errors[0].statusText}</p>
+              <p className="text-xs">
+                {errors[0].data.msg}
+              </p>
+            </div>:<></>}
+          <form className="block  h-fit" onSubmit={handleSubmit}>
+            <div className="mb-6">
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Enter password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+              />
+            </div>
+            <div className="flex justify-end items-center">
+            {(verifying)?<i className="fas fa-spin fa-spinner h-fit mr-4"></i>:<></>}
+            <button
+              type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
+            </div>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Table.jsx
@@ -0,0 +1,122 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const Table = (props) => {
+	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	const handleDelete = (e) => {
+		axios.post(`${props.baseUrl}/sublist/delete?id=${e.target.id}`, {},{
+			headers: {
+				Authorization: localStorage.getItem("token")
+			}
+		})
+		.then((res) => {
+			console.log(res)
+			console.log("Entry deleted from database")
+			setData(data.filter((row) => row.id !== parseInt(e.target.id)))
+		})
+		.catch((err) => {
+			console.log(err)
+			console.log("Delete operation failed")
+		})
+
+	}
+
+	const fetchData = () => {
+		console.log("fetching data");
+		setLoading(true);
+		axios
+			.get(`${props.baseUrl}/sublist`, {
+				headers: {
+					Authorization: localStorage.getItem("token"),
+				},
+			})
+			.then((res) => {
+				console.log(res);
+				setData(res.data.sublist);
+				setLoading(false);
+			})
+			.catch((err) => {
+				console.log(err)
+				if (err || err.response.status === 401) {
+					setLoading(false);
+				}
+			});
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, [data.length === 0]);
+
+	return (
+		<>
+			{loading ? (
+				<i className="fas fa-spin fa-spinner fa-2x"></i>
+			) : data.length === 0 ? (
+				<div
+					className="bg-blue-100 rounded border border-blue-500 text-blue-700 px-4 py-3"
+					role="alert"
+				>
+					<p className="font-bold">No data available</p>
+					<p className="text-sm">
+						Click the refresh button below to check for data.
+					</p>
+					<button
+						onClick={fetchData}
+						className=" mt-3 w-1/3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+					>
+						Refresh
+					</button>
+				</div>
+			) : (
+				<div className="max-w-500">
+					<div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+						<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+							<thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+								<tr>
+									<th scope="col" className="px-6 py-3">
+										Sl. No.
+									</th>
+									<th scope="col" className="px-6 py-3">
+										Name
+									</th>
+									<th scope="col" className="px-6 py-3">
+										Email ID
+									</th>
+									<th scope="col" className="px-6 py-3">
+										Action
+									</th>
+								</tr>
+							</thead>
+							<tbody>
+								{data.map((s, i) => (
+									<tr
+										key={i}
+										className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+									>
+										<td
+											scope="row"
+											className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+										>
+											{i+1}
+										</td>
+										<td className="px-6 py-4">{s.name}</td>
+										<td className="px-6 py-4">{s.email}</td>
+										<td className="px-6 py-4">
+											<button onClick={handleDelete} id={s.id} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+												Delete
+											</button>
+										</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</div>
+				</div>
+			)}
+		</>
+	);
+};
+
+export default Table;
